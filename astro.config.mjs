8d58d80b27e56af1c1f89628221f4033c7b749e1
kaungSethLwin <ikaungsettlwindev@gmail.com>
2025-09-13 17:24:59 +0700

    fix the personal infos

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kaungsethlwin.github.io',
+  site: 'https://kaungsettlwin.github.io',
   base: '/kaungsettlwin-portfolio', 
   output: 'static'
 });
